78325b922a496f0bdb973c623ccdaf42eea702b3
Rafli Raihan Permana <rafliraihan245@gmail.com>
2023-12-09 13:07:57 +0700

    kalkulator first commit

---
+++ src/Kalkulator.java
@@ -0,0 +1,15 @@
+
+public class Kalkulator {
+
+    public static void main(String[] args) {
+
+        // membuat objek untuk class kedua
+        KalkulatorClass main = new KalkulatorClass();
+
+
+        // memanggil function kalkulator dari class
+        main.kalkulator();
+
+    }
+
+}
---
+++ src/KalkulatorClass.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class KalkulatorClass {
+
+    public void kalkulator() {
+
+        int angkaPertama;
+        int angkaKedua;
+        Scanner inputUser = new Scanner(System.in);
+        System.out.println(" Kalkulator");
+
+        System.out.println(" Masukkan Angka");
+        System.out.print("\n Angka pertama: ");
+        angkaPertama = inputUser.nextInt();
+        System.out.print(" Angka kedua: ");
+        angkaKedua = inputUser.nextInt();
+
+        // Function untuk menentukan operasi hitung
+        System.out.println("\n Pilih salah satu karakter operasi hitung (+,-,*,/) : ");
+        String operasi = inputUser.next();
+
+
+        switch (operasi) {
+            case "+":
+                System.out.println(" Hasil: "+ ( angkaPertama + angkaKedua ));
+                break;
+            case "-":
+                System.out.println(" Hasil: "+ ( angkaPertama - angkaKedua ));
+                break;
+            case "*":
+                System.out.println(" Hasil: "+ ( angkaPertama * angkaKedua ));
+                break;
+            case "/":
+                System.out.println(" Hasil: "+ ( angkaPertama / angkaKedua ));
+                break;
+            default: System.out.println(" Harap pilih karakter");
+        }
+    }
+}
